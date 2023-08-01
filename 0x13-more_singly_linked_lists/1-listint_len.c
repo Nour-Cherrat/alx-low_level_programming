83fcbda11bdf6040fae5cb3e7b4ff9cbb92d3392
Nour-Cherrat <60980128+Nour-Cherrat@users.noreply.github.com>
2023-08-02 00:05:47 +0100

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,14 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t count = 0;
+    size_t x = 0;
 
     while (h != NULL)
     {
         h = h->next;
-        count++;
+        x++;
     }
 
-    return count;
+    return x;
 }
-
