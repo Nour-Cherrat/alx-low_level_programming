9778bf2b29f51e40592e597ebdd7f02d1d65d757
Nour-Cherrat <cherrat.nour@gmail.com>
2023-08-03 15:21:24 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,19 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
+
 	if (b == NULL)
-		return 0;
-	
+		return (0);
+
 	while (*b != '\0')
 	{
 		if (*b != '0' && *b != '1')
-			return 0;
+			return (0);
+
 		result = result * 2 + (*b - '0');
 		b++;
 	}
-	return result;
+
+	return (result);
 }
+
