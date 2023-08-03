23dd95cf02e7f5c8ebb0de05e5d7d805bc7c0290
Nour-Cherrat <cherrat.nour@gmail.com>
2023-08-03 15:34:01 -0700

    10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,27 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * get_bit - returns the value of a bit at a given
- * index.
- * @n: unsigned long int input.
- * @index: index of the bit.
+ * get_bit - function that returns the value of a bit at a given index.
+ * @n: number to scan.
+ * @index: bit to return.
  *
- * Return: value of the bit.
+ * Return: Bit (0 | 1)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i;
+	int bit;
 
-	if (n == 0 && index < 64)
-		return (0);
-
-	for (i = 0; i <= 63; n >>= 1, i++)
-	{
-		if (index == i)
-		{
-			return (n & 1);
-		}
-	}
-
-	return (-1);
+	bit = ((n >> index) & 1);
+	if (index > 64)
+		return (-1);
+	return (bit);
 }
+
