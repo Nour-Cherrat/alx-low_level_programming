45d8354bbcfd9d4594b7c968aada23078760f3ec
Nour-Cherrat <60980128+Nour-Cherrat@users.noreply.github.com>
2023-08-03 23:32:26 +0100

    10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,18 +1,27 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * get_bit - Returns the value of a bit at a given index.
- * @n: The number to extract the bit from.
- * @index: The index of the bit to get, starting from 0 (LSB).
+ * get_bit - returns the value of a bit at a given
+ * index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
  *
- * Return: The value of the bit at the specified index, or -1 if an error occurs.
+ * Return: value of the bit.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit;
+	unsigned int i;
 
-	bit = ((n >> index) & 1);
-	if (index > 64)
-		return (-1);
-	return (bit);
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+
+	return (-1);
 }
